5471b8174ab02f6d4298924453bfbf4bcd2e9e32
sagarkakkar03 <122864251+sagarkakkar03@users.noreply.github.com>
2023-10-16 21:14:27 +0530

    Add files via upload

--- ToDo/tasks/forms.py
+++ ToDo/tasks/forms.py
@@ -3,6 +3,7 @@ from django import forms
 from django.forms import ModelForm
 
 class TaskForm(forms.ModelForm):
+    title = forms.CharField(widget=forms.TextInput(attrs={'placeholder': 'Add new task...'}))
     class Meta:
         model = Task
         fields = '__all__'
